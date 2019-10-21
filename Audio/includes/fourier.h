919e2047fd1babe1b513dcc1c44077adf062b546
Caio <caiomarcoschaves@usp.br>
2019-10-21 12:16:38 -0300

    Criando funcao pra calcular a dft

--- Audio/includes/fourier.h
+++ Audio/includes/fourier.h
@@ -1,9 +1,13 @@
 #ifndef
 #define
 
-complex double *dft();
+//calcula a transformada discreta de fourier
+complex double *dft(unsigned char *, int);
+//calcula a inversa da transformada discreta de fourier
 unsigned char *dft_inversa();
+//reduz a magnitude do coeficiente
 double *reduz_coeficiente();
+//zera a magnitude do coeficiente
 void zera_coeficiente();
 
 
--- Audio/src/fourier.c
+++ Audio/src/fourier.c
@@ -3,3 +3,30 @@
 #include <file.h>
 #include <math.h>
 #include <complex.h>
+
+void troca(double *vetor, int pos0, int pos1){
+	double aux;
+	aux = vetor[pos0];
+	vetor[pos0] = vetor[pos1];
+	vetor[pos1] = aux;
+}
+
+complex double *dft(unsigned char *valor, int N){
+	int k, t;
+	double x;
+	complex double *coeficiente;
+	coeficiente = (complex double *) malloc((N / 2 + 1) * sizeof(complex double *));
+	complex double z;
+	for(k = 0; k <= N/2; k++){
+		z = 0;
+		for(t = 0; t < N; t++){
+			x = 2 * k * acos(-1) * (t / N);
+			z += cexp(-1 * x * I) * valor[t];
+		}
+		if(k == 0)
+			coeficiente[k] = (1.0 / N) * z;
+		else
+			coeficiente[k] = (2.0 / N) * z;
+	}
+	return coeficiente;
+}
