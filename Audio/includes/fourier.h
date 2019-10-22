9e5b393db41f1bb46eaf62152823af5120ff00c0
Caio <caiomarcoschaves@usp.br>
2019-10-22 11:44:19 -0300

    Inversa da DFT feita, organizando coeficientes

--- Audio/includes/fourier.h
+++ Audio/includes/fourier.h
@@ -4,9 +4,9 @@
 //calcula a transformada discreta de fourier
 complex double *dft(unsigned char *, int);
 //calcula a inversa da transformada discreta de fourier
-unsigned char *dft_inversa();
+unsigned char *dft_inversa(complex double *, int);
 //reduz a magnitude do coeficiente
-double *reduz_coeficiente();
+double *reduz_coeficiente(complex double *, int *, int);
 //zera a magnitude do coeficiente
 void zera_coeficiente();
 
--- Audio/src/fourier.c
+++ Audio/src/fourier.c
@@ -4,24 +4,17 @@
 #include <math.h>
 #include <complex.h>
 
-void troca(double *vetor, int pos0, int pos1){
-	double aux;
-	aux = vetor[pos0];
-	vetor[pos0] = vetor[pos1];
-	vetor[pos1] = aux;
-}
-
 complex double *dft(unsigned char *valor, int N){
 	int k, t;
-	double x;
+	double exp; //parte do expoente que sera elevado em e
 	complex double *coeficiente;
-	coeficiente = (complex double *) malloc((N / 2 + 1) * sizeof(complex double *));
-	complex double z;
+	coeficiente = (complex double *) malloc((N / 2 + 1) * sizeof(complex double));
+	complex double z; //resultado do somatorio
 	for(k = 0; k <= N/2; k++){
 		z = 0;
 		for(t = 0; t < N; t++){
-			x = 2 * k * acos(-1) * (t / N);
-			z += cexp(-1 * x * I) * valor[t];
+			exp = 2 * k * acos(-1) * (t / N); //Tambem eh usado o arccos de -1 para ter o valor de pi
+			z += cexp(-1 * I * exp) * valor[t];
 		}
 		if(k == 0)
 			coeficiente[k] = (1.0 / N) * z;
@@ -29,4 +22,46 @@ complex double *dft(unsigned char *valor, int N){
 			coeficiente[k] = (2.0 / N) * z;
 	}
 	return coeficiente;
+}
+
+unsigned char *dft_inversa(complex double *coeficiente, int N){
+	int k, t;
+	double exp; //parte do expoente que sera elevado em e
+	unsigned char *valor;
+	valor = (unsigned char *) malloc((N + 1) * sizeof(unsigned char));
+	complex double z; //resultado do somatorio
+	for(k = 0; k <= N; k++){
+		z = 0;
+		for(t = 0; t <= N/2; t++){
+			exp = 2 * k * acos(-1) * (t / N); //Tambem eh usado o arccos de -1 para ter o valor de pi
+			z += cexp(I * exp) * coeficiente[t];
+		}
+		valor[k] = (unsigned char) round(creal(z)); // Arredondando valor para cima quando parte decimal eh >= 5
+	}
+	return valor;
+}
+
+void troca(double *vetor, int pos0, int pos1){
+	double aux;
+	aux = vetor[pos0];
+	vetor[pos0] = vetor[pos1];
+	vetor[pos1] = aux;
+}
+
+double *reduz_coeficiente(complex double *coeficiente, int *places, int N){
+	int i, j, aux;
+	double *mags = malloc(sizeof(double)*(N+1));
+	for(i = 0; i <= N; i++)
+		mags[i] = cabs(inducts[i]);
+	for(i = 0; i <= N; i++){
+		for(j = 1; j <= N-i; j++){
+			if(mags[j] > mags[j-1]){
+				troca(mags, j, j-1);
+				aux = places[j-1];
+				places[j-1] = places[j];
+				places[j] = aux;
+			}
+		}
+	}
+	return mags;
 }
