9d5f9c7548dd568425216166ac20c313b30b2cd8
Caio <caiomarcoschaves@usp.br>
2019-10-24 14:18:46 -0300

    Estruturando melhor o código

--- Labirinto/includes/grafo.h
+++ Labirinto/includes/grafo.h
@@ -1,12 +1,16 @@
 #ifndef _GRAFO_H_
 #define _GRAFO_H_
+struct grafo_elem{
+	float x, y;
+	int 
+}
+typedef struct grafo_elem GRAFO_ELEM;
+
 struct grafo{
+	GRAFO_ELEM *origem;
 	int vertices; //os vertices sao os numeros de pontos
-	int x, y
-	int arcos;
-	int **adj;
-	double distancia;
+	int **adj; //se eh um caminho, o valor da matriz eh 1, se nao eh 0
+	int arcos; //numero de caminhos
 }
 typedef struct grafo GRAFO;
-
 #endif
