571c32c643c1c2fc6a2f84336c147bce45cdaeb6
Caio <caiomarcoschaves@usp.br>
2019-10-25 00:07:14 -0300

    Começando código do Tensor Esparso

---
+++ Tensor-Esparso/includes/defines.h
@@ -0,0 +1,11 @@
+#ifndef _DEFINES_H_
+#define _DEFINES_H_
+
+#define SUCCESS		 0
+#define INVALID_PLANE 	-1
+#define REMOVE_ERROR 	-2
+#define INVALID_CUBE 	-3
+#define INVALID_POS	-4
+#define INVALID_KEY     -5
+
+#endif
---
+++ Tensor-Esparso/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <complex_sparse_cube.h>
+
+int main(void){
+
+
+
+
+	return 0;
+}
