8fbf9a5e79a3771f120ce88902a3f06dfcea7f47
Caio <caiomarcoschaves@usp.br>
2019-10-29 18:01:46 -0300

    Structs feitas

---
+++ Labirinto/includes/defines.h
@@ -0,0 +1,14 @@
+#ifndef _DEFINES_H_
+#define _DEFINES_H_
+
+#define TRUE		 1
+#define FALSE		 0
+
+#define SUCCESS		 0
+#define INVALID_GRAPH 	-1
+#define REMOVE_ERROR 	-2
+#define INVALID_POS	-4
+#define INVALID_KEY     -5
+#define INVALID_STACK  	-6
+
+#endif
---
+++ Labirinto/includes/graph.h
@@ -0,0 +1,22 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+struct graph_elem{
+	float x, y;
+	int isCam;
+	int isExit; 
+	int connections; //Numero de conexoes que esse vertice tem
+	struct graph_elem **next; //Ponteiro duplo pois pode haver mais de um vertice conectado a outro
+				  //Conexoes ordenadas por ordem numerica
+}
+typedef struct graph_elem GRAPH_ELEM;
+
+struct graph{
+	GRAPH_ELEM *first;
+	int vertices; //os vertices sao os numeros de pontos
+	int **adj; //se eh um caminho, o valor da matriz eh 1, se nao eh 0
+	int paths; //numero de caminhos
+}
+typedef struct graph GRAPH;
+
+
+#endif
