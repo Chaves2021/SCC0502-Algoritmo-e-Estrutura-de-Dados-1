6fcc31b4c277c152c0547b322056e803747a4ea8
Viana2021 <caiomarcoschaves@gmail.com>
2020-01-07 21:02:56 -0300

    Adicionando código

--- Anagrama/src/hash.c
+++ Anagrama/src/hash.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <hash.h>
+#include <defines.h>
 
 HASH_TABLE *hash_create(){
 	HASH_TABLE *h = (HASH_TABLE *) calloc(1, sizeof(HASH_TABLE));
 	return h;
+}
+
+int hash_add(HASH_TABLE *, int value){
 }
